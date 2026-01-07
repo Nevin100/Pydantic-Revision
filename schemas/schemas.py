a16136240d06f05ee56b92a050c7c174c62d3d41
Nevin100 <opopopopop100000@gmail.com>
2026-01-07 18:26:48 +0530

    Revising Pydantic

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+from typing import List, Optional
 # Models for the Patient entity
 class PatientModel(BaseModel):
     id: str
@@ -8,3 +8,11 @@ class PatientModel(BaseModel):
     age: int
 
 # Here, all the above fields are mandatory. If any field is missing, Pydantic will raise a validation error.
+
+class PatientsModel(BaseModel):
+    id: str
+    name: str
+    city: str
+    age: int
+    diseases: List[str] = None # Optional in nature and if not given, then by default the value is None
+    address: Optional[str]
